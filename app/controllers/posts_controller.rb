e546e28608a18bdb0cac3182d96e42faa8cb6606
roro1053 <c1053rroro@gmail.com>
2020-11-27 18:51:06 +0900

    投稿が保存される

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,27 @@
 class PostsController < ApplicationController
 
   def index
+    
   end
    
   def new
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+    @post.save
+    redirect_to root_path(@post)
+    else
+      render :new
+    end
+  end
+
+private
+
+def post_params
+  params.require(:post).permit(:text,:image).merge(user_id: current_user.id)
+end
 
 end
