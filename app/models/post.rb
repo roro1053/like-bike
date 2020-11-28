9d10fd4e4212db9193dcf9bc4e4cfe5760f7638c
roro1053 <c1053rroro@gmail.com>
2020-11-28 12:46:39 +0900

    画像かテキストのどちらかを入力するようにバリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :text, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
