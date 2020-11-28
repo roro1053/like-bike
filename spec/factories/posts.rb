f908cde704766d0dd290bd43fadde51a26ae3de2
roro1053 <c1053rroro@gmail.com>
2020-11-28 16:38:52 +0900

    postモデルの単体テストコードを確認

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :post do
-    
-  end
+    text { "text" }
+    association :user 
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+ end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+   @post = FactoryBot.build(:post)
+  end
+  
+  describe "新規投稿" do
+    context "新規投稿がうまく行く時" do
+      it "textとimageが存在すれば投稿できる" do
+        expect(@post).to be_valid
+      end
+      it "imageが存在すればtextが空でも投稿できる" do
+        @post.text = ""
+        expect(@post).to be_valid
+      end
+      it "textが存在すればimageが空でも投稿できる" do
+        @post.image = nil
+        expect(@post).to be_valid
+      end
+    end
+
+    context "新規投稿がうまくいかない時" do
+      it "imageとtextが空だと投稿できない" do
+        @post.image = nil
+        @post.text = ""
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Text can't be blank")
+      end
+    end
+  end
 end
