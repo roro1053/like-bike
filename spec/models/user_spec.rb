1bbeeafcf6c6380c1bded25e7563b055886ad87a
roro1053 <c1053rroro@gmail.com>
2020-11-29 12:04:07 +0900

    userモデルの正常系テストコードを確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,12 +7,37 @@ RSpec.describe User, type: :model do
 
    describe "新規登録" do
     context "登録がうまく行く時" do
-      it "nickname,email,passwordが存在すれば登録できる" do
+      it "nickname,email,password,profile,imageが存在すれば登録できる" do
         expect(@user).to be_valid
       end
+      it "nicknameが40文字以内なら登録できる" do
+        @user.nickname = "1234567890123456789012345678901234567890"
+        expect(@user).to be_valid
+      end
+      it "passwordが6文字以上で尚且つ、英数混合なら登録できる" do
+        @user.password = "12345a"
+        @user.password_confirmation = "12345a"
+        expect(@user).to be_valid
+      end
+      it "profileが空でも登録できる" do
+        @user.profile = ""
+        expect(@user).to be_valid
+      end
+      it "imageが空でも登録できる" do
+        @user.image = nil
+        expect(@user).to be_valid
+      end
+      it "profileとimageが空でも登録できる" do
+        @user.profile = ""
+        @user.image = nil
+        expect(@user).to be_valid
+      end
+
      end
     context "登録がうまくいかない時" do
-      
+       it "" do
+        
+      end
     end
    end
 end
