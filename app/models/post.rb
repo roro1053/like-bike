126e9a248c904df913b549b06aa634e43df9fbde
roro1053 <c1053rroro@gmail.com>
2020-11-30 12:10:41 +0900

    新規投稿の追加テストコードを確認した

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :text, presence: true, unless: :was_attached?,
-                     length: { maximum: 150 }
+  validates :text,length: { maximum: 150 }
+  validates :text,presence: true, unless: :was_attached?
+ 
 
   def was_attached?
     self.image.attached?
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -27,6 +27,17 @@ RSpec.describe Post, type: :model do
         @post.valid?
         expect(@post.errors.full_messages).to include("Text can't be blank")
       end
+      it "imageが存在せず、textが151文字以上だと投稿できない" do
+        @post.image = nil
+        @post.text = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901"
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Text is too long (maximum is 150 characters)")
+      end
+      it "imageは存在するが、textが151文字以上だと投稿できない" do
+        @post.text = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901"
+        @post.valid?
+        expect(@post.errors.full_messages).to include(["Text is too long (maximum is 150 characters)")
+      end
     end
   end
 end
