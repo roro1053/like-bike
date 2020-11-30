6ca8262cfee505da3805432064e780071f05cf56
roro1053 <c1053rroro@gmail.com>
2020-11-30 12:25:51 +0900

    投稿者でないユーザーが編集ページにアクセスすると、トップページに遷移する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:edit, :show, :update, :destroy]
+
 
   def index
     @posts = Post.includes(:user).order('created_at DESC')
@@ -33,6 +35,10 @@ class PostsController < ApplicationController
   end
 
   def edit
+    unless user_signed_in? && current_user.id == @post.user_id
+      redirect_to action: :index
+      return
+    end
   end
 
   def update
