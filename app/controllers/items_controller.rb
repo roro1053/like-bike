b4b3a7b133b0ad26c31b002ece9185ace093138c
roro1053 <c1053rroro@gmail.com>
2020-12-04 17:03:18 +0900

    レビュー（テキスト）が保存される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @review = Review.new
   end
 
   private
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    if @review.valid?
+      @review.save
+    redirect_to "/items/#{@review.item.id}"
+    else 
+      @post = @comment.post
+      @comments = @post.comments
+      render "posts/show"
+    end
+  end
+  private 
+  def review_params
+    params.require(:review).permit(:text,:score).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
