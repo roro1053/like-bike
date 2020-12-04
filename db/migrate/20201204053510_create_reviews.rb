d704ecd8522b6e01a8d4fb34908d34b2dc69f0eb
roro1053 <c1053rroro@gmail.com>
2020-12-04 14:39:17 +0900

    reviewモデルとマイグレーションファイルを作成

---
+++ db/migrate/20201204053510_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.references :user, foreign_key: true
+      t.references :item, foreign_key: true
+      t.text :text
+      t.integer :score
+      t.timestamps
+    end
+  end
+end
