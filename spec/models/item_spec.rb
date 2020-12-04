80d4a95593925dfbe6ccbbb9f68a99cb8f055a5b
roro1053 <c1053rroro@gmail.com>
2020-12-04 11:30:02 +0900

    itemモデルのテストコードを作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+   end
+   describe "アイテム投稿" do
+    context "アイテム投稿がうまくいく時" do
+      it "name,text,imageが存在すれば投稿できる" do
+        expect(@item).to be_valid
+      end
+      it "" do
+        
+      end
+      it "" do
+        
+      end
+      it "" do
+        
+      end
+
+    end
+    context "アイテム投稿がうまくいかない時" do
+      
+    end
+    
+   end
 end
