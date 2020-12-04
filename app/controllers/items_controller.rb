24fd2925567593d72b9a73360f6fb9acc194eea6
roro1053 <c1053rroro@gmail.com>
2020-12-04 14:19:36 +0900

    アイテム一覧機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
-    @item = Item.includes(:user).order('created_at DESC')
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
