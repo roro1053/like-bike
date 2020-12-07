e183c29d0f50319d30076537dca558b4a0b4fd5c
roro1053 <c1053rroro@gmail.com>
2020-12-07 20:57:12 +0900

    ユーザーの詳細ページを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
 
  def show
-  user = User.find(params[:id])
-  @nickname = user.nickname
-  @posts = user.posts.includes(:user).order('created_at DESC')
+  @user = User.find(params[:id])
+  @nickname = @user.nickname
+  @posts = @user.posts.includes(:user).order('created_at DESC')
  end
 
  def item
-  user = User.find(params[:id])
-  @nickname = user.nickname
-  @items = user.items.includes(:user).order('created_at DESC')
+  @user = User.find(params[:id])
+  @nickname = @user.nickname
+  @items = @user.items.includes(:user).order('created_at DESC')
  end
 
 end
