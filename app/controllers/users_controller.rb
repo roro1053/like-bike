d0c34d08fed829706a51fe6d95aed4adaadcb3c2
roro1053 <c1053rroro@gmail.com>
2020-12-07 17:41:11 +0900

    userのitem一覧ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
 
-def show
+ def show
   user = User.find(params[:id])
   @nickname = user.nickname
   @posts = user.posts.includes(:user).order('created_at DESC')
-end
+ end
 
+ def item
+  user = User.find(params[:id])
+  @nickname = user.nickname
+  @items = user.items.includes(:user).order('created_at DESC')
+ end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    member do
+      get 'item'
+    end
+  end
   resources :items, only: [:index,:new,:create,:show] do
     resources :reviews, only: [:index,:create,:destroy]
   end
