52fac22347b48ff32eae121f0648ecf164e5f863
roro1053 <c1053rroro@gmail.com>
2020-12-08 11:42:40 +0900

    item_tag_relationモデルとマイグレーションファイルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_053510) do
+ActiveRecord::Schema.define(version: 2020_12_08_023922) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2020_12_04_053510) do
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "item_tag_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_item_tag_relations_on_item_id"
+    t.index ["tag_id"], name: "index_item_tag_relations_on_tag_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -90,6 +99,8 @@ ActiveRecord::Schema.define(version: 2020_12_04_053510) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "item_tag_relations", "items"
+  add_foreign_key "item_tag_relations", "tags"
   add_foreign_key "items", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "reviews", "items"
