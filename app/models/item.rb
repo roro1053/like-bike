a330d9955fbf5539cbf47570aa820b09d200f7dc
roro1053 <c1053rroro@gmail.com>
2020-12-08 11:46:21 +0900

    アソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  has_many :tags
+  has_many :item_tag_relations
+  has_many :tags, through: :item_tag_relations
   has_many :reviews, dependent: :destroy
 
   with_options presence: true do
