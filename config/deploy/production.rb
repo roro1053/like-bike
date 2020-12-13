59e4d6bd5e39df46b9f7a4efbb38858f28b0e39d
roro1053 <c1053rroro@gmail.com>
2020-12-13 22:13:02 +0900

    コミット

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server ' 13.112.128.186 ', user: 'ec2-user', roles: %w{app db web}
+server '13.112.128.186', user: 'ec2-user', roles: %w{app db web}
