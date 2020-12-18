42d968a1c7ab134143b2233f0e3ca2af635ebafe
roro1053 <c1053rroro@gmail.com>
2020-12-18 12:23:05 +0900

    リファクタリング

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,18 @@
 class LikesController < ApplicationController
+  before_action :set_like, only: [:create, :destroy]
 
   def create
-    @post = Post.find(params[:post_id])
     @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
     @likes = Like.where(post_id: params[:post_id])
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
     @like.destroy
   end
 
+  private
+  def set_like
+    @post = Post.find(params[:post_id])
+  end
 end
