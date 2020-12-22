142c39d7c9c1f79058e51510cc16a879bf9e8f06
roro1053 <c1053rroro@gmail.com>
2020-12-22 20:51:01 +0900

    comment結合テスト

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -108,14 +108,56 @@ RSpec.describe 'コメント削除', type: :system do
        expect(page).to have_content @comment.text
        #削除ボタンを確認する
        expect(page).to have_content "削除する"
-       
+       #ログアウトする
+       click_link('ログアウト')
+       #別のユーザーでログインする
+       visit new_user_session_path
+       fill_in 'Email', with: @user.email
+       fill_in 'Password', with: @user.password
+       find('input[name="commit"]').click
+       expect(current_path).to eq root_path
+       #投稿詳細ページにアクセスする
+       visit post_path(@post)
+       #コメントの存在を確認する
+       expect(page).to have_selector(".comment-image[src$='test_image.png']")
+       expect(page).to have_content @comment.text
+       #削除するボタンが存在しないことを確認する
+       expect(page).to have_no_content("削除する")
     end
     it "ログアウトユーザーには削除ボタンが表示されない" do
-      
+      #コメント1を投稿したユーザーがログインする
+      visit new_user_session_path
+      fill_in 'Email', with: @comment.user.email
+      fill_in 'Password', with: @comment.user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq root_path
+      #投稿の詳細ページに遷移する
+      visit post_path(@post)
+      #フォームに情報を入力する
+      fill_in 'comment_text', with: @comment.text
+      attach_file "comment_image", "public/images/test_image.png"
+      # コメントを送信すると、Commentモデルのカウントが1上がることを確認する
+      expect{
+        click_button('SEND')
+      }.to change { Comment.count }.by(1)
+      # 詳細ページにリダイレクトされることを確認する
+      expect(current_path).to eq post_path(@post)
+      # 詳細ページ上に先ほどのコメント内容が含まれていることを確認する
+      expect(page).to have_selector(".comment-image[src$='test_image.png']")
+      expect(page).to have_content @comment.text
+      #削除ボタンを確認する
+      expect(page).to have_content "削除する"
+      #ログアウトする
+      click_link('ログアウト')
+      #投稿詳細ページにアクセスする
+      visit post_path(@post)
+      #コメントの存在を確認する
+      expect(page).to have_selector(".comment-image[src$='test_image.png']")
+      expect(page).to have_content @comment.text
+      #削除するボタンが存在しないことを確認する
+      expect(page).to have_no_content("削除する")
     end
   end
-
-
 end
 
  
