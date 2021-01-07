2b6229ccf3a77e0fef48553a863ff414c00f2093
roro1053 <c1053rroro@gmail.com>
2021-01-07 10:18:31 +0900

    tagモデルのアソシエーションを修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :item_tag_relations
+  has_many :item_tag_relations, dependent: :destroy
   has_many :items, through: :item_tag_relations
 
   validates :word, uniqueness: true,length: { maximum: 40 }
