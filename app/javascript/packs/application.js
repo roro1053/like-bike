bb36618449958d61aac3e2f8a10eca85a25b3c48
roro1053 <c1053rroro@gmail.com>
2021-01-14 17:52:06 +0900

    require rails-ujsを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,7 +17,7 @@ require('jquery')
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-//= require rails-ujs
+
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
